219df5b595187459ef152caf0a10c8eb24c34f72
Ulysse ARNAUD <mexanga@gmail.com>
2020-05-26 15:01:23 +0200

    Fixed all bugs associated to webpack & npm

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -11,47 +11,41 @@ const nodeExternals = require('webpack-node-externals');
 
 const DEFAULT_SERVER = './server/server.js';
 
-module.exports = (
-    env,
-    argv
-) => {
-    return (
-        {
-            entry: {
-                server: DEFAULT_SERVER
-            },
-            output: {
-                path: path.join(
-                    __dirname,
-                    'dist'
-                ),
-                publicPath: '/',
-                filename: '[name].js'
-            },
-            mode: argv.mode,
-            target: 'node',
-            node: {
-                // Need this when working with express, otherwise the build fails
-                __dirname: false,   // if you don't put this is, __dirname
-                __filename: false   // and __filename return blank or /
-            },
-            externals: [nodeExternals()], // Need this to avoid error when
-                                          // working with Express
-            module: {
-                rules: [
-                    {
-                        // Transpiles ES6-8 into ES5
-                        test: /\.js$/,
-                        exclude: /node_modules/,
-                        use: {
-                            loader: 'babel-loader'
-                        }
+module.exports =
+    {
+        entry: {
+            server: DEFAULT_SERVER
+        },
+        output: {
+            path: path.join(
+                __dirname,
+                'dist'
+            ),
+            publicPath: '/',
+            filename: '[name].js'
+        },
+        mode: 'development',
+        target: 'node',
+        node: {
+            // Need this when working with express, otherwise the build fails
+            __dirname: false,   // if you don't put this is, __dirname
+            __filename: false   // and __filename return blank or /
+        },
+        externals: [nodeExternals()], // Need this to avoid error when
+                                      // working with Express
+        module: {
+            rules: [
+                {
+                    // Transpiles ES6-8 into ES5
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
                     }
-                ]
-            }
+                }
+            ]
         }
-    );
-};
+    };
 
 // Webpack 4 basic tutorial:
 // https://www.valentinog.com/blog/webpack-4-tutorial/#webpack_4_production_and_development_mode
