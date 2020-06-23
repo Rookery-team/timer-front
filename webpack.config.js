6106d7608f92016075ae2fe8ca7007f7cf2530a9
Ulysse ARNAUD <mexanga@gmail.com>
2020-06-23 18:57:01 +0200

    Edited mode from dev to prod in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         publicPath: '/',
         filename: '[name].js'
     },
-    mode: 'development',
+    mode: 'production',
     target: 'web',
     // devtool: '#source-map',
     devtool: '#source-map',
